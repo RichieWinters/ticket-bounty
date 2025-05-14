ee496f6c796dd2f11f97eb1af35f90553e5e64d6
Urban Vadzim <Herr.Wallace@protonmail.com>
2025-05-14 23:58:26 +0300

    feat: add styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,17 @@ import { ticketsPath } from '@/tickets';
 
 const HomePage = () => {
   return (
-    <div>
-      <h2>Home Page </h2>
-      <Link href={ticketsPath()} className='underline'>
-        Go to tickets
-      </Link>
+    <div className='flex flex-col gap-12'>
+      <div>
+        <h2 className='text-3xl font-bold tracking-tight'>Home Page </h2>
+        <p className='text-sm'>Your starting point</p>
+      </div>
+
+      <div className='flex flex-col items-center flex-1'>
+        <Link href={ticketsPath()} className='underline'>
+          Go to tickets
+        </Link>
+      </div>
     </div>
   );
 };
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,16 +4,27 @@ import { ticketPath } from '@/tickets';
 
 const TicketsPage = () => {
   return (
-    <div>
-      {initTickets.map((ticket) => (
-        <div key={ticket.id}>
-          <h2 className='text-lg'>{ticket.title}</h2>
+    <div className='flex flex-col gap-12'>
+      <div>
+        <h2 className='text-3xl font-bold tracking-tight'>Tickets Page</h2>
+        <p className='text-sm'>Your tickets are in here</p>
+      </div>
 
-          <Link href={ticketPath(ticket.id)} className='text-sm underline'>
-            View
-          </Link>
-        </div>
-      ))}
+      <div className='flex flex-col items-center flex-1 gap-6'>
+        {initTickets.map((ticket) => (
+          <div
+            key={ticket.id}
+            className='w-full max-w-[350px] p-4 border border-neutral-300 rounded'
+          >
+            <h3 className='text-lg font-semibold truncate'>{ticket.title}</h3>
+            <p className='text-sm truncate text-neutral-500'>{ticket.content}</p>
+
+            <Link href={ticketPath(ticket.id)} className='text-sm underline'>
+              View
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
